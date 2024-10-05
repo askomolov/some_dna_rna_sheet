ca2541f772478362ba2cf6fabc1e6b8f09c5e87e
Komolov Aleksandr <askomolov@mail.ru>
2024-10-05 17:46:46 +0300

    Add annotations

--- modules/filter_fastq_func.py
+++ modules/filter_fastq_func.py
@@ -1,4 +1,5 @@
-def check_params(**kwargs):   # Функция проверяет введены ли параметры фильтрации. Если нет, то задаёт значения по умолчанию.
+def check_params(**kwargs) -> dict:   # Функция проверяет введены ли параметры фильтрации. Если нет, то задаёт значения по умолчанию.
+    "Set parameters for selection (default): gc_bounds (0,100), length_bounds(0, 2**32), quality_threshold (0)"
     gc_b = kwargs.get('gc_bounds')
     if gc_b is None:
         gc_b_low = 0
@@ -26,7 +27,8 @@ def check_params(**kwargs):   # Функция проверяет введены
     return params1
 
 
-def seqs_choose(seqs, params):
+def seqs_choose(seqs: dict, params: dict) -> dict:
+    "Sequence choose by parameters"
     new_seqs = {}
     for name, seq in seqs.items():
         gc_comp = gc_comp_calc(seq[0])
@@ -37,11 +39,13 @@ def seqs_choose(seqs, params):
     return new_seqs
 
 
-def gc_comp_calc(seq):
+def gc_comp_calc(seq: str) -> float:
+    "Calculate GC composition of sequence (in%)"
     return (seq.count('G')+seq.count('C'))/len(seq)*100
 
 
-def quality_calc(seq_q):
+def quality_calc(seq_q: str) -> float:
+    "Calculate average quality"
     quality = 0
     for i in range(0, len(seq_q)):
         quality += ord(seq_q[i])-33
--- some_dna_rna_sheet.py
+++ some_dna_rna_sheet.py
@@ -3,9 +3,20 @@ import modules.filter_fastq_func as fifa
 
 
 def run_dna_rna_tools(*args):
+    """
+    Script wth some tools for DNA and RNA sequences
+    Input: One or more DNA or RNA sequences and one action from the list: transcribe, reverse, complement, reverse_complement
+    If no DNA or RNA sequences or not nucleotide sequence in input return None.
+    If action is:
+    1) transcribe - function transcibe all DNA sequence. If RNA in input return None.
+    2) reverse - function reverse nucleotide sequence.
+    3) complement - function make complementary DNA sequence.
+    4) reverse_complement - function make reverse complementary DNA sequence.
+    5) else - function return None.
+    """
     *seqs, proc = args
     new_seqs = []
-    if drt.input_check(seqs) == 0:
+    if drt.input_check(seqs) is False:
         return None
     for i in range(0, len(seqs)):
         if proc == 'transcribe':
@@ -26,7 +37,11 @@ def run_dna_rna_tools(*args):
         return new_seqs
 
 
-def filter_fastq(seqs, **kwargs):
+def filter_fastq(seqs: dict, **kwargs) -> dict:
+    """
+    Filter input reads.
+    Parameters for filtration could be specified (default): gc_bounds (0,100), length_bounds(0, 2**32), quality_threshold (0).
+    """
     params = fifa.check_params(**kwargs)  # Записывыем все параметры фильтраций (введенные или по умолчанию) в один словарь
     act_seqs = fifa.seqs_choose(seqs, params)  # Выбор последовательностей, подходящих по параметрам
     return act_seqs
